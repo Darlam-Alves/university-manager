36288f72fdab961658b5a3718c187de47340ceae
darlam <darlamalvessilva@gmail.com>
2025-03-26 17:15:31 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to university-manager
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/university_manager/core.clj
@@ -0,0 +1,7 @@
+(ns university-manager.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
